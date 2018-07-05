0627405fc2e9bfeee310af5829fd6f1f0cabacc6
Benjaprogramado <40808786+Benjaprogramado@users.noreply.github.com>
2018-07-04 22:34:01 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad= document.getElementById("edad").value;
+edad= parseInt(edad);
+
+//opcion con "and"
+
+//if (edad>=13 && edad<=17);
+
+//Opcion con "not"
+
+if (!(edad<13 || edad>17));
+
+{
+    alert ("Es Adolecente")
+}
 
 
 }//FIN DE LA FUNCIÓN
