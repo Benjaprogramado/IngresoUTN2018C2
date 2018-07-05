d1edf92ae5af67482cee5d4b8affda9a0d2f7bd3
Benjaprogramado <40808786+Benjaprogramado@users.noreply.github.com>
2018-07-04 21:40:57 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+
+var importe;
+var descuento;
+var porcentaje;
+
+importe= document.getElementById("importe").value;
+//importe= parseInt(importe);
+
+porcentaje= importe * 0.25;
+//porcentaje=parseInt(porcentaje);
+
+descuento= importe-porcentaje;
+//descuento=parseInt(descuento);
+
+document.getElementById("resultado").value= descuento  
+
+
 	
 }
--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,19 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad= document.getElementById("edad").value;
+edad=parseInt(edad);
+
+if (edad == 15)
+{
+    alert ("Niña Bonita")
+}
 
 }//FIN DE LA FUNCIÓN
+
+/*Cuando utilizamos "=" le asignamos valor a un objeto, con doble "==" nos referimos a una comparación
+como en este caso, y si utilizamos triple igual "==="(en java) la igualdad comparada es completa, es decir,
+en este caso tanto el dato 15 como su formato (string, number, etc) deberia ser idéntico.*/
+ 
