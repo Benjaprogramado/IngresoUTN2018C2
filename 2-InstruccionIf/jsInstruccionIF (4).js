f7d66b0b1f1731d476d23fa11f1f6493a093f8be
Benjaprogramado <40808786+Benjaprogramado@users.noreply.github.com>
2018-07-10 20:50:07 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -7,17 +7,29 @@ var edad;
 edad= document.getElementById("edad").value;
 edad= parseInt(edad);
 
-//opcion con "and"
+//Opcion con "and", se debe cumplir el rango especificado y devuelve el alert indicado para la funciòn if.
 
-//if (edad>=13 && edad<=17);
+//if (edad >=13 && edad<=17) 
 
-//Opcion con "not"
 
-if (!(edad<13 || edad>17));
+//Opcion con "not", si se cumple lo especificado, se niega, entonces lo invierte y da verdadero.
+
+if (!(edad<13 || edad>17))
+
+
 
 {
-    alert ("Es Adolecente")
+
+alert ("Es Adolecente")
+
 }
+    
+
+//Opcion con "not", si se cumple lo especificado, se niega, entonces lo invierte.
+
+//if (!(edad<13 || edad>17))
 
 
-}//FIN DE LA FUNCIÓN
+
+}
+//FIN DE LA FUNCIÓN
