9641a06a59740e37cbef0eceed42af65ff42bccb
Benjaprogramado <40808786+Benjaprogramado@users.noreply.github.com>
2018-07-10 21:25:07 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,6 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+var estadoCivil
+
+edad= parseInt(document.getElementById("edad").value);
+estadoCivil= document.getElementById("estadoCivil").value;
+
+if (edad<18 && estadoCivil!="Soltero")
+{
+    alert ("Demasiado chico para no ser Soltero")
+}
+
 	
 
 
