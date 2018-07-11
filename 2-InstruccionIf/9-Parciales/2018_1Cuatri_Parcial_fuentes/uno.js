c062f3f5752dba2865238feff308eeb08a56597a
Benjaprogramado <40808786+Benjaprogramado@users.noreply.github.com>
2018-07-10 22:18:04 -0300

    Add files via upload

---
+++ 2-InstruccionIf/9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -0,0 +1,18 @@
+
+function mostrar()
+{
+var ancho;
+var largo;
+var perímetro;
+
+ancho= prompt ("Ingrese Ancho");
+ancho=parseInt(ancho);
+largo= prompt ("Ingrese Largo");
+largo=parseInt(largo);
+perímetro= ancho+largo+ancho+largo;
+
+alert ("El perímetro del rectángulo es "+perímetro)
+
+
+
+}
