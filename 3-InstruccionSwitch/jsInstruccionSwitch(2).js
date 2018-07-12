4be86b583bc3ce16bcf658741c1e43b33c53244d
Benjaprogramado <40808786+Benjaprogramado@users.noreply.github.com>
2018-07-11 22:16:46 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,68 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+/*var mesDelAño = document.getElementById("mes").value;
 
-alert (mesDelAño);
+switch (mesDelAño){
+
+    
+
+    case "Enero":
+    alert ("Todavia falta para el invierno");
+    break
+    case "Febrero":
+    alert ("Todavia falta para el invierno");
+    break
+    case "Marzo":
+    alert ("Todavia falta para el invierno");
+    break
+    case "Abril":
+    alert ("Todavia falta para el invierno");
+    break
+    case "Mayo":
+    alert ("Todavia falta para el invierno");
+    break
+    case "Junio":
+    alert ("Todavia falta para el invierno");
+    break
+    case "Julio":
+    alert ("Abrigate hace frio");
+    break
+    case "Agosto":
+    alert ("Abrigate hace frio");
+    break
+    default: 
+    alert ("Ya pasamos el frio, ahora el calor");
+    break
+  */
+
+
+
+  //Otra resoluciòn mas óptima y reducida sería:
+
+var mesDelAño = document.getElementById("mes").value;
+
+switch (mesDelAño){
+
+    
+
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    alert ("Todavia falta para el invierno");
+    break
+    case "Julio":
+    case "Agosto":
+    alert ("Abrigate hace frio");
+    break
+    default: 
+    alert ("Ya pasamos el frio, ahora el calor");
+    break
+  
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,26 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño = document.getElementById("mes").value;
 
-//alert (mesDelAño);
+switch (mesDelAño) {
+
+
+}
+
+
+
+/*
+Meses de 28 días: 
+Febrero tiene 28 salvo en los años bisiestos que tiene 29 días. 
+
+Meses de 30 días: 
+Abril, junio, septiembre y noviembre. 
+
+Meses de 31 días: 
+Enero, marzo, mayo, julio, agosto, octubre y diciembre. */
+
+
 	
 	
 
