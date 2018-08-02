b0942ba872644fe92f334e1d3aaf30b76807ed1c
Benjaprogramado <40808786+Benjaprogramado@users.noreply.github.com>
2018-08-01 22:10:12 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,41 @@
 function mostrar()
 {
 
+var contPares=0;
+var contImpares=0;
+var contCeros=0;
+var promedioPos
+var acumPos=0;
+var contPos=0;
+var acumNeg=0;
+var maximo;
+var minimo;
+var letra;
+var numero;
+var respuesta;
+
+do{
+    
+    numero=parseInt(prompt("Ingrese un número;"));
+    while(numero<-100 || numero>100 || isNaN(numero)){
+
+        numero=parseInt(prompt("ERROR, Reingrese el número;"));
+
+    }
+    letra=prompt("Ingrese una letra:");
+
+if(numero%2==0){
+    contPares++;
+}
+else{
+    contImpares++;
+}
+
+
+
+
+respuesta=prompt("Desea continuar??");
+}while(respuesta=="si");
+
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
 
+var nota;
+var sexo;
+
+for(contador=0;contador<5;contador++){
+
+    do{
+        nota=parseInt(prompt("Ingrese Nota"));
+
+    } while(nota<0 ||nota>10);
+
+    do{
+       sexo=parseInt(prompt("Ingrese sexo")); 
+    }while(sexo!=m&&sexo!="f);
+
+
+//TERMINAR, HAY FOTOS DE AYUDA TAMBIEN
+
+} 
+
 }
